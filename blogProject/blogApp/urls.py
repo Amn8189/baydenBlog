db525a29280c3ed7894f2b5971de7a5389fca5b5
Amn8189 <aydenmunga@gmail.com>
2024-07-03 19:01:48 +0300

    error with reloading

--- blogProject/blogApp/urls.py
+++ blogProject/blogApp/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import Homepage, Article_content
+from .views import Homepage, Article_content, search
 
 urlpatterns = [
     path('', Homepage, name="homepage"),
     path('article/<int:id>', Article_content, name="article"),
+    path('search/', search, name="search"),
 ]
--- blogProject/blogApp/views.py
+++ blogProject/blogApp/views.py
@@ -9,3 +9,8 @@ def Homepage(request):
 def Article_content(request, id):
     article = Article.objects.get(pk=id)
     return render(request, 'content.html', {"article": article})
+
+def search(request):
+    query = request.POST.get("query")
+    filtered_articles = Article.objects.filter(title__contains=query)
+    return render(request, "search.html", {"article": filtered_articles})
